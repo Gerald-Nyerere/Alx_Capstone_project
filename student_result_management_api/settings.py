3cff8b8c1c7641a42a6fea2e599841ab575b9a66
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-29 14:01:10 +0300

    updated my requirements.txt

--- student_result_management_api/settings.py
+++ student_result_management_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!3-*&e8+c4-zmf(374vr6hzwgfawk2x6#in@a&@z9z9-0wpe1-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['student-result-management-api.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
