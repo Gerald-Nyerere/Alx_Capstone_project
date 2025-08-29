76a1f5808a206df260e236d9f8d0799929868a9f
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-29 19:23:09 +0300

    Update Django settings for pythonanywhere deployment

--- student_result_management_api/settings.py
+++ student_result_management_api/settings.py
@@ -46,11 +46,14 @@ INSTALLED_APPS = [
     'results',
     'classrooms', 
     'rest_framework',
-    'rest_framework.authtoken',   
+    'rest_framework.authtoken',
+    'whitenoise',
+       
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise Gzip & Brotli compression for better performance
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
