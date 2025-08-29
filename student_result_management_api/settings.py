ee8f1335c9cb8249d5050951c5f808284d6cb2e9
gerald-nyerer <geraldnyerer100@gmail.com>
2025-08-29 16:45:50 +0300

    Update Django settings for pythonanywhere deployment

--- student_result_management_api/settings.py
+++ student_result_management_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!3-*&e8+c4-zmf(374vr6hzwgfawk2x6#in@a&@z9z9-0wpe1-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["gerald19.pythonanywhere.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
